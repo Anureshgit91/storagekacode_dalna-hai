044fa213c4f28f5a034aa273fd010acde15ccf50
Anureshgit91 <anuresh89@gmail.com>
2025-07-13 21:25:06 +0530

    story dala hu

--- main.tf
+++ main.tf
@@ -11,4 +11,12 @@ resource "azurerm_storage_account" "stg432" {
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
   public_network_access_enabled = false
+  
 }
+
+ कहानी: "बांस और फर्न की सीख"
+एक बार की बात है, एक आदमी बहुत परेशान था। वो रोज़ मेहनत करता था, पर उसे कोई सफलता नहीं मिल रही थी। वो थक गया था।
+
+वो एक संत के पास गया और बोला:
+
+"गुरुजी, मैं सबकुछ कर रहा हूँ – मेहनत, लगन, पर फिर भी मेरी ज़िंदगी में कोई बदलाव नहीं आ रहा। मैं अब हार मानना चाहता हूँ।"
