98d1bd708a3f79fedd16cced780da27afae0baa3
Anureshgit91 <anuresh89@gmail.com>
2025-07-13 21:41:47 +0530

    msg

--- main.tf
+++ main.tf
@@ -12,3 +12,9 @@ resource "azurerm_storage_account" "stg432" {
   account_replication_type = var.account_replication_type
   public_network_access_enabled = false
 }
+सीख (Moral):
+"ज़िंदगी में कई बार मेहनत का फल जल्दी नहीं मिलता।
+लेकिन इसका मतलब ये नहीं कि मेहनत बेकार जा रही है।
+हो सकता है कि ज़मीन के नीचे तुम्हारी जड़ें मजबूत हो रही हों –
+ताकी तुम एक दिन बहुत ऊँचा उठ सको।
+बस, रुको मत – मेहनत करते रहो।"
